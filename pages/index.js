0f382da08be964b79a22450343f02d1a1e77fcfa
Caleb <choover11@gmail.com>
2020-05-19 14:40:05 -0700

    first question

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
-// import useSWR from 'swr';
-// import fetch from 'isomorphic-unfetch';
+import { VictoryPie } from 'victory';
+
+function Question({ title, visualization }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      {visualization}
+    </div>
+  )
+}
+
+const histogram = (data, column) => {
+  const responses = {};
+  data.slice(1).forEach(row => {
+    const res = row[column];
+    if (!res) return;
+    if (!responses[res]) {
+      responses[res] = 1;
+    } else {
+      responses[res]++;
+    }
+  });
+  return Object.keys(responses).map(i => ({
+    x: i,
+    y: responses[i],
+  }));
+}
 
 function HomePage({ foo }) {
   const [data, setData] = React.useState(null)
@@ -33,16 +58,29 @@ function HomePage({ foo }) {
     return <div/>;
   }
 
-  console.log(14, data);
+  console.log(14, histogram(data, 0));
 
   return (
     <div>
-      {data[0].map((q, i) => (
-        <div>
-          <h3>{q}</h3>
-          <p>{data.slice(1).map(r => r[i]).join(', ')}</p>
-        </div>
-      ))}
+      <Question
+        title={data[0][0]}
+        visualization={(
+          <div style={{
+            width: '50%',
+            height: '50%'
+          }}>
+            <VictoryPie
+              data={histogram(data, 0)}
+              responsive="true"
+              style={{
+                labels: {
+                  fontSize: '8px'
+                }
+              }}
+            />
+          </div>
+        )}
+      />
     </div>
   );
 }
